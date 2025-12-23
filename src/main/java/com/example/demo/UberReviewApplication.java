c197282db97f42bb9b243cddd5ac6617e0362759
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-12-23 17:52:05 +0530

    basic spring data jpa config done

---
+++ src/main/java/com/example/demo/UberReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberReviewApplication.class, args);
+	}
+
+}
