060370a1d3a2aa3fadc16cdf2f9612ad5513ea50
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-12-24 21:41:30 +0530

    Added basemodel class using mappedsuperclass

---
+++ src/main/java/com/example/demo/models/BaseClass.java
@@ -0,0 +1,29 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+}
--- src/main/java/com/example/demo/models/Review.java
+++ src/main/java/com/example/demo/models/Review.java
@@ -1,15 +1,8 @@
 package com.example.demo.models;
 
 import jakarta.persistence.*;
-import jdk.jfr.Timestamp;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -18,23 +11,14 @@ import java.util.Date;
 @AllArgsConstructor
 @Entity
 @Table(name = "booking_review")
-@EntityListeners(AuditingEntityListener.class)
-public class Review {
+public class Review extends BaseClass {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
 
     @Column(nullable = false)
     private String content;
 
     private Double ratings;
 
-    @CreatedDate
-    @Column(name = "created_at", updatable = false, nullable = false)
-    private LocalDate createdAt;
 
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDate updatedAt;
 }
